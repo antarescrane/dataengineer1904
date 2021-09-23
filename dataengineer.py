a3b8a732e41638cab4c0ae7b760a69a13bece9a7
antarescrane <41974906+antarescrane@users.noreply.github.com>
2021-09-23 17:53:50 -0500

    Add files via upload
    nextlargestnumber

---
+++ dataengineer.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def rearrange_bigger(n):
+    #Break the number into digits and store in a list
+    nums = list(str(n))
+    for i in range(len(nums)-2,-1,-1):
+        if nums[i] < nums[i+1]:
+            z = nums[i:]
+            y = min(filter(lambda x: x > z[0], z))
+            z.remove(y)
+            z.sort()
+            nums[i:] = [y] + z
+            return int("".join(nums))
+    return False
+
+print(rearrange_bigger(1200011111))
